64ae0d33c77885d32835934e4fff0c35f009da58
GraphicNovel <onegoseen@gmail.com>
2021-01-31 16:43:40 +0900

    Update HomeTemplate.js
    - MusicPlayerBar가 토글됨에 따라 (open) visibility가 변경되는 animation 적용

--- src/component/HomeTemplate.js
+++ src/component/HomeTemplate.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import MusicList from './MusicList';
-import styled from 'styled-components';
-import { useMusicState } from '../MusicContext';
+import styled, { keyframes } from 'styled-components';
 
+const ContentHidden = keyframes`
+    0%{
+        visibility: visible;
+    }
+    100%{
+        visibility: hidden;
+    }
+`;
+
+const ContentVisible = keyframes`
+    0%{
+        visibility: hidden;
+    }
+    100%{
+        visibility: visible;
+    }
+`;
 
 const HomeTemplateStyle = styled.div`
     width:80%;
     margin:0 auto;
+
+    visibility: visible;
+
+    animation: ${props => props.open ? ContentHidden : ContentVisible} 0.5s forwards;
 `;
-function HomeTemplate({ subtitle, title }) {
+
+function HomeTemplate({ subtitle, title, open }) {
     return (
-        <HomeTemplateStyle>
-            <p>{subtitle}</p>
-            <h1>{title}</h1>
-            <MusicList />
-        </HomeTemplateStyle>
+        <>
+            {open && <HomeTemplateStyle open>
+                <p>{subtitle}</p>
+                <h1>{title}</h1>
+                <MusicList />
+            </HomeTemplateStyle>}
+            { !open && <HomeTemplateStyle>
+                <p>{subtitle}</p>
+                <h1>{title}</h1>
+                <MusicList />
+            </HomeTemplateStyle>}
+        </>
     );
 };
 
