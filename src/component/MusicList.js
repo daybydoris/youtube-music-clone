7035e700f77f63ebb9e977061a54b4c90660ac42
GraphicNovel <onegoseen@gmail.com>
2021-02-17 19:19:10 +0900

    add: install 'react-slick' and apply it on 'MusicList' component

--- src/component/MusicList.js
+++ src/component/MusicList.js
@@ -2,11 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import MusicItem from './MusicItem';
 import { useMusicState } from '../MusicContext';
+import Slider from "react-slick";
 
 
 const MusicListBlock = styled.div`
-    display:flex;
+    .slick-track{
+    }
+    .slick-slide{
+        min-width:220px;
+    }
 `;
+
+const settings = {
+    arrows: true,
+    dots: false,
+    infinite: false,
+    slidesToShow: 5
+}
 
 function MusicList({ onOpenPop, myMusicPop, localIndex }) {
 
@@ -14,21 +26,24 @@ function MusicList({ onOpenPop, myMusicPop, localIndex }) {
 
     return (
         <MusicListBlock>
-            {
-                songs.map(song => (
-                    <MusicItem
-                        key={song.id}
-                        id={song.id}
-                        title={song.title}
-                        artist={song.artist}
-                        thumb={song.thumb}
-                        url={song.url}
-                        nowPlaying={song.nowPlaying}
-                        onOpenPop={onOpenPop}
-                        myMusicPop={myMusicPop}
-                        localIndex={localIndex}
-                    />
-                ))}
+            <Slider {...settings}>
+                {
+                    songs.map(song => (
+                        <MusicItem
+                            key={song.id}
+                            id={song.id}
+                            title={song.title}
+                            artist={song.artist}
+                            thumb={song.thumb}
+                            url={song.url}
+                            nowPlaying={song.nowPlaying}
+                            copyright={song.copyright}
+                            onOpenPop={onOpenPop}
+                            myMusicPop={myMusicPop}
+                            localIndex={localIndex}
+                        />
+                    ))}
+            </Slider>
         </MusicListBlock>
     );
 };
