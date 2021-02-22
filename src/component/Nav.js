32f53c0ae3832a4071dbe767829b7ced3f0b64e1
GraphicNovel <onegoseen@gmail.com>
2021-02-22 19:15:47 +0900

    add: profile
    - 프로필 영역 스타일링

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -39,25 +39,89 @@ const SearchButton = styled.div`
     cursor:pointer;
 `;
 
+const Profile = styled.div`
+    width:30px;
+    height:30px;
+
+    background: #846523;
+    border-radius:50%;
+    cursor: pointer;
+
+    color: #fff;
+    font-weight: bold;
+
+    position: relative;
+
+    text-align: center;
+    line-height:1.7;
+`;
+
+const ProfileInfo = styled.div`
+    display: flex;
+    align-items: center;
+    backgound: #212121;
+
+    width:100%;
+    height: 36px;
+    padding:0 8px;
+`;
+
+const ProfileOption = styled.ul`
+    position: absolute;
+    right:0; top: 100%;
+
+    display: ${props => props.profile ? "flex" : "none"};
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    width: 180px;
+    margin: 0;
+    padding: 12px 0;
+    background:#212121;
+
+    z-index: 10;
+
+    border: 1px solid rgba(255,255,255,0.1);
+    border-radius: 2px;
+
+    font-weight: 300;
+`;
+
+const ProfileOptionList = styled.li`
+    display: flex;
+    align-items: center;
+    backgound: #212121;
+
+    width:100%;
+    height: 36px;
+    padding:0 8px;
+
+    cursor: pointer;
+
+    &:hover{
+        background:#323232;
+    }
+`;
+
 const activeStyle = {
     color: "#fff"
 }
 
 function Nav({ onClosePop }) {
     const [open, setOpen] = useState(false);
+    const [profile, setProfile] = useState(false);
 
-    const onSearchOpen = () => {
-        setOpen(true);
-    }
+    const onSearchOpen = () => { setOpen(true); }
 
-    const onSearchClose = () => {
-        setOpen(false);
-    }
+    const onSearchClose = () => { setOpen(false); }
+
+    const onProfileToggle = () => { setProfile(!profile); }
 
     return (
         <NavContainer>
             <div className="logo">
-                <Link to="/" onClick={onClosePop}><img src="https://music.youtube.com/img/on_platform_logo_dark.svg" alt="" /></Link>
+                <Link to="/" onClick={onClosePop}><img src="logo_dark.png" alt="" width="80px" height="24px"/></Link>
             </div>
             <MenuContainer>
                 <NavLink to="/" onClick={onClosePop} activeStyle={activeStyle} exact>
@@ -74,7 +138,9 @@ function Nav({ onClosePop }) {
                 </SearchButton>
                 {open && <SearchBox onSearchClose={onSearchClose} />}
             </MenuContainer>
-            <div className="profile">프로필</div>
+            <Profile className="profile">
+                G
+            </Profile>
         </NavContainer >
     );
 };
